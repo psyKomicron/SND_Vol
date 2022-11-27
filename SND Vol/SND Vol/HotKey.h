03cfe559d2fefd9f3330fefdcc3c2d164a33caee
psykomicron <psykomicron@protonmail.com>
2022-11-27 14:50:26 +0100

    Activate now throws invalid_argument
    If the key cannot be registered by the notification thread, the Activate function will throw invalid_argument on call.

--- SND Vol/SND Vol/HotKey.h
+++ SND Vol/SND Vol/HotKey.h
@@ -10,19 +10,36 @@ namespace System
 		HotKey(const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& key);
 		~HotKey();
 
+		/**
+		 * @brief Key modifiers (control, alt, etc...).
+		 * @return Virtual key modifiers
+		*/
 		inline winrt::Windows::System::VirtualKeyModifiers KeyModifiers()
 		{
 			return TranslateModifiers(modifiers);
 		};
+
+		/**
+		 * @brief Key.
+		 * @return The VK_[key name] associated with this hotkey
+		*/
 		inline uint32_t Key()
 		{
 			return key;
 		};
 
+		/**
+		 * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
+		 * @return true if the hotkey is enabled
+		*/
 		inline bool Enabled()
 		{
 			return keyEnabled.load();
 		};
+		/**
+		 * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
+		 * @param enabled set to true to disable events
+		*/
 		inline void Enabled(const bool& enabled)
 		{
 			keyEnabled.exchange(enabled);
@@ -45,14 +62,14 @@ namespace System
 	private:
 		static std::atomic_int32_t id;
 
-		uint32_t modifiers;
-		uint32_t key;
+		uint32_t modifiers = 0u;
+		uint32_t key = 0u;
 		std::atomic_flag threadFlag{};
 		int32_t hotKeyId = 0;
 		std::thread* notificationThread = nullptr;
 		std::atomic_bool threadRunning = false;
 		std::atomic_bool keyEnabled = true;
-		DWORD threadId;
+		DWORD threadId = 0ul;
 
 		winrt::event<winrt::Windows::Foundation::TypedEventHandler<winrt::Windows::Foundation::IInspectable, winrt::guid>> e_fired{};
 
