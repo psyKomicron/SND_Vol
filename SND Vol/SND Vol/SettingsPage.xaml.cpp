871141589604ac9e8f461355af52d72dd1abb8fe
psykomicron <psykomicron@protonmail.com>
2022-12-08 00:37:38 +0100

    Added new settings, updated UI
    - New transparency settings.
    - Fixed "Custom title bar" & "Transparency effects" (swapped).
    - Added app background image suppport when not using transparency effects.

--- SND Vol/SND Vol/SettingsPage.xaml.cpp
+++ SND Vol/SND Vol/SettingsPage.xaml.cpp
@@ -5,9 +5,13 @@
 #endif
 
 using namespace winrt;
-using namespace Microsoft::UI::Xaml;
+using namespace winrt::Microsoft::UI::Xaml;
 using namespace winrt::Microsoft::UI::Xaml::Navigation;
 using namespace winrt::Windows::UI::Xaml::Interop;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::Storage::Pickers;
 
 
 namespace winrt::SND_Vol::implementation
@@ -20,6 +24,14 @@ namespace winrt::SND_Vol::implementation
         AppVersionTextBlock().Text(
             to_hstring(packageId.Version().Major) + L"." + to_hstring(packageId.Version().Minor) + L"." + to_hstring(packageId.Version().Build)
         );
+    }
+
+    IAsyncAction SettingsPage::Page_Loaded(IInspectable const&, RoutedEventArgs const&)
+    {
+        TransparencyEffectsToggleButton().IsOn(unbox_value_or(ApplicationData::Current().LocalSettings().Values().TryLookup(L"TransparencyAllowed"), true));
+        CustomTitleBarToggleButton().IsOn(unbox_value_or(ApplicationData::Current().LocalSettings().Values().TryLookup(L"UseCustomTitleBar"), true));
+        StartupTask startupTask = co_await StartupTask::GetAsync(L"CroakStartupTaskId");
+        AddToStartupToggleSwitch().IsOn(startupTask.State() == StartupTaskState::Enabled);
     }
 
     void SettingsPage::OnNavigatedTo(NavigationEventArgs const& args)
@@ -48,5 +60,65 @@ namespace winrt::SND_Vol::implementation
     void SettingsPage::NewContentButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
         Frame().Navigate(xaml_typename<NewContentPage>());
+    }
+
+    void SettingsPage::TransparencyEffectsToggleButton_Toggled(IInspectable const&, RoutedEventArgs const&)
+    {
+        ApplicationData::Current().LocalSettings().Values().Insert(L"TransparencyAllowed", IReference(TransparencyEffectsToggleButton().IsOn()));
+    }
+
+    void SettingsPage::CustomTitleBarToggleButton_Toggled(IInspectable const&, RoutedEventArgs const&)
+    {
+        ApplicationData::Current().LocalSettings().Values().Insert(L"UseCustomTitleBar", IReference(CustomTitleBarToggleButton().IsOn()));
+    }
+
+    IAsyncAction SettingsPage::AddToStartupToggleSwitch_Toggled(IInspectable const&, RoutedEventArgs const&)
+    {
+        AddToStartupProgressRing().Visibility(Visibility::Visible);
+        StartupTask startupTask = co_await StartupTask::GetAsync(L"CroakStartupTaskId");
+        if (AddToStartupToggleSwitch().IsOn())
+        {
+            // Add app to startup.
+            if (startupTask.State() == StartupTaskState::Disabled)
+            {
+                StartupTaskState newState = co_await startupTask.RequestEnableAsync();
+                if (newState == StartupTaskState::Enabled)
+                {
+                    UserMessageBar().EnqueueMessage(L"App added to startup");
+                }
+            }
+        }
+        else
+        {
+            // Remove app from startup.
+            if (startupTask.State() == StartupTaskState::Enabled)
+            {
+                // Disable task
+                startupTask.Disable();
+                UserMessageBar().EnqueueMessage(L"App removed from startup");
+            }
+        }
+
+        AddToStartupProgressRing().Visibility(Visibility::Collapsed);
+    }
+
+    IAsyncAction SettingsPage::BrowseButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        FileOpenPicker picker{};
+
+        // HACK: https://github.com/microsoft/WindowsAppSDK/issues/1063
+        HWND mainWindowHandle = GetWindowFromWindowId(SecondWindow::Current().Id());
+        picker.as<IInitializeWithWindow>()->Initialize(mainWindowHandle);
+
+        picker.FileTypeFilter().ReplaceAll({ L"*" });
+        picker.SettingsIdentifier(L"Spotlight Importer");
+        picker.SuggestedStartLocation(PickerLocationId::PicturesLibrary);
+        picker.ViewMode(PickerViewMode::List);
+
+        auto chosenFile = co_await picker.PickSingleFileAsync();
+        if (chosenFile)
+        {
+            ApplicationData::Current().LocalSettings().Values().Insert(L"BackgroundImageUri", box_value(chosenFile.Path()));
+        }
     }
 }
--- SND Vol/SND Vol/SettingsPage.xaml.h
+++ SND Vol/SND Vol/SettingsPage.xaml.h
@@ -9,9 +9,14 @@ namespace winrt::SND_Vol::implementation
         SettingsPage();
 
         void OnNavigatedTo(winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs const& args);
+        winrt::Windows::Foundation::IAsyncAction Page_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void AudioProfilesButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void HotKeysButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void NewContentButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void TransparencyEffectsToggleButton_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void CustomTitleBarToggleButton_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        winrt::Windows::Foundation::IAsyncAction AddToStartupToggleSwitch_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        winrt::Windows::Foundation::IAsyncAction BrowseButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
     };
 }
 
