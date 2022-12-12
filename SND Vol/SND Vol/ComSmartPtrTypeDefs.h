e38ecd914fbe9296122d2766e9a96af30245680c
psykomicron <psykomicron@protonmail.com>
2022-12-12 19:00:41 +0100

    Create ComSmartPtrTypeDefs.h

---
+++ SND Vol/SND Vol/ComSmartPtrTypeDefs.h
@@ -0,0 +1,36 @@
+#pragma once
+
+_COM_SMARTPTR_TYPEDEF(IStream, __uuidof(IStream));
+
+
+// IMMDevicePtr
+_COM_SMARTPTR_TYPEDEF(IMMDevice, __uuidof(IMMDevice));
+// IMMDeviceEnumeratorPtr
+_COM_SMARTPTR_TYPEDEF(IMMDeviceEnumerator, __uuidof(IMMDeviceEnumerator));
+// IAudioSessionManager2Ptr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionManager2, __uuidof(IAudioSessionManager2));
+// IAudioSessionManager2Ptr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionManager2, __uuidof(IAudioSessionManager2));
+// IAudioSessionEnumeratorPtr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionEnumerator, __uuidof(IAudioSessionEnumerator));
+// IAudioSessionControl2Ptr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionControl2, __uuidof(IAudioSessionControl2));
+// IAudioSessionControlPtr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionControl, __uuidof(IAudioSessionControl));
+// ISimpleAudioVolumePtr
+_COM_SMARTPTR_TYPEDEF(ISimpleAudioVolume, __uuidof(ISimpleAudioVolume));
+// IAudioSessionNotificationPtr
+_COM_SMARTPTR_TYPEDEF(IAudioSessionNotification, __uuidof(IAudioSessionNotification));
+// IAudioEndpointVolumePtr
+_COM_SMARTPTR_TYPEDEF(IAudioEndpointVolume, __uuidof(IAudioEndpointVolume));
+// IAudioMeterInformationPtr
+_COM_SMARTPTR_TYPEDEF(IAudioMeterInformation, __uuidof(IAudioMeterInformation));
+
+
+// appxpackaging.h
+_COM_SMARTPTR_TYPEDEF(IAppxFactory, __uuidof(IAppxFactory));
+_COM_SMARTPTR_TYPEDEF(IAppxPackageReader, __uuidof(IAppxPackageReader));
+_COM_SMARTPTR_TYPEDEF(IAppxManifestReader, __uuidof(IAppxManifestReader));
+_COM_SMARTPTR_TYPEDEF(IAppxManifestApplicationsEnumerator, __uuidof(IAppxManifestApplicationsEnumerator));
+_COM_SMARTPTR_TYPEDEF(IAppxManifestProperties, __uuidof(IAppxManifestProperties));
+_COM_SMARTPTR_TYPEDEF(IAppxManifestApplication, __uuidof(IAppxManifestApplication));
